3220586590e7382070fdbbe29ce0ef03b048f742
Girish Gupta <girish@girishgupta.com>
2025-12-24 08:29:46 -0800

    Embed

---
+++ embed.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from jaxtyping import Float, Int, Tensor
+
+
+class EmbedWithoutTorch(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_E = nn.Parameter(torch.empty((cfg.d_vocab, cfg.d_model)))
+        nn.init.normal_(self.W_E, std=self.cfg.init_range)
+
+    def forward(
+        self, tokens: Int[Tensor, "batch position"]
+    ) -> Float[Tensor, "batch position d_model"]:
+        return self.W_E[tokens]
+
+
+class EmbedWithTorch(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.embedding = nn.Embedding(cfg.d_vocab, cfg.d_model)
+        nn.init.normal_(self.embedding.weight, std=self.cfg.init_range)
+
+    def forward(
+        self, tokens: Int[Tensor, "batch position"]
+    ) -> Float[Tensor, "batch position d_model"]:
+        return self.embedding(tokens)
--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import einops
 from dataclasses import dataclass
 from jaxtyping import Float, Tensor
 from layernorm import LayerNormWithEinops, LayerNormWithoutEinops, LayerNormWithTorch
+from embed import EmbedWithoutTorch, EmbedWithTorch
 
 device = torch.device(
     "mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu"
