1ff8aa9bab86c9f411a7b5f31e0dd0751b0f9a20
Girish Gupta <girish@girishgupta.com>
2025-12-24 08:40:47 -0800

    Move config

--- train.py
+++ train.py
@@ -6,8 +6,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import einops
-from dataclasses import dataclass
 from jaxtyping import Float, Tensor
+from config import GPTConfig
 from layernorm import LayerNormWithEinops, LayerNormWithoutEinops, LayerNormWithTorch
 from embed import EmbedWithoutTorch, EmbedWithTorch
 from positional_embedding import PosEmbedWithEinops, PosEmbedWithoutEinops
@@ -20,17 +20,3 @@ device = torch.device(
     "mps" if torch.backends.mps.is_available(
     ) else "cuda" if torch.cuda.is_available() else "cpu"
 )
-
-
-@dataclass
-class GPTConfig:
-    d_model: int = 768
-    debug: bool = True
-    layer_norm_eps: float = 1e-5
-    d_vocab: int = 50257
-    init_range: float = 0.02
-    n_ctx: int = 1024
-    d_head: int = 64
-    d_mlp: int = 3072
-    n_heads: int = 12
-    n_layers: int = 12
