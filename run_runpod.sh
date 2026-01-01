585e6febd44c195df47d191314e97acdeb530de9
Girish Gupta <girish@girishgupta.com>
2026-01-01 08:47:15 -0800

    Correct filename

--- run_runpod.sh
+++ run_runpod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 pip install uv
-uv run --with streamlit streamlit run Home.py --server.port=8501 --server.address=0.0.0.0 --server.enableWebsocketCompression=false --server.enableCORS=false
+uv run --with streamlit streamlit run main.py --server.port=8501 --server.address=0.0.0.0 --server.enableWebsocketCompression=false --server.enableCORS=false
